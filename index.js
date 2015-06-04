b07271eaced98532302b23a0c949e7b567f86cb1
Adam Crabtree <dude@noderiety.com>
2015-06-04 13:44:34 -0700

    Add extraneous arguments to circumvent callback detection

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var nodeify = require('bluebird-nodeify')
 
 module.exports = function nodeifyit(promiseReturningFunction, options) {
-  return function() {
+  // Provide extraneous arguments to circumvent callback detection
+  return function(a, b, c, d, e, f, g) {
     var callback = arguments[arguments.length-1]
     var promise = promiseReturningFunction.apply(this, arguments)
     nodeify(promise, callback, options)
